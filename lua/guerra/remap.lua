571db646b4c8fa62afe633b77c2a3368dcc32169
Israel Guerra <israel.guerra@gmail.com>
2024-04-01 09:28:30 -0300

    testing some custom mappings

--- lua/guerra/remap.lua
+++ lua/guerra/remap.lua
@@ -151,6 +151,20 @@ wk.register({
       end,
       'Localtion List',
     },
+
+    -- ['{'] = { function()
+    --   vim.cmd.normal('}ztj')
+    --   local current_line = vim.api.nvim_get_current_line()
+    --   if current_line:match('^%s*$') ~= nil then
+    --     vim.cmd.normal('"_dd')
+    --   end
+    -- end, 'Jump to next block of code'
+    -- },
+
+    -- ['}'] = { function()
+    --   vim.cmd.normal('{{jzt')
+    -- end, 'Jump to previous block of code'
+    -- },
   },
 }, { prefix = '<leader>' })
 
