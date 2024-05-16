41c32d93d411896ab3ff3ae83b78dedfca9217e0
Israel Guerra <israel.guerra@gmail.com>
2024-05-15 23:08:07 -0300

    resize disabled

--- lua/guerra/plugins/nvim-tree.lua
+++ lua/guerra/plugins/nvim-tree.lua
@@ -3,8 +3,14 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup({
+      actions = {
+        open_file = {
+          resize_window = false
+        }
+      },
       view = {
         side = "right",
+        width = 50,
       },
     })
   end
