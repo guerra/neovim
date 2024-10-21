975416b420553ffa120cb32023b63e461fc777bd
Israel Guerra <israel.guerra@gmail.com>
2024-10-20 21:08:56 -0300

    harpoon split

--- lua/guerra/plugins/harpoon.lua
+++ lua/guerra/plugins/harpoon.lua
@@ -26,6 +26,18 @@ return {
       }):find()
     end
 
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        vim.keymap.set("n", "<C-v>", function()
+          harpoon.ui:select_menu_item({ vsplit = true })
+        end, { buffer = cx.bufnr })
+
+        vim.keymap.set("n", "<C-x>", function()
+          harpoon.ui:select_menu_item({ split = true })
+        end, { buffer = cx.bufnr })
+      end,
+    })
+
     require('which-key').add(
       {
         mode = { "n" },
