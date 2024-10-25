aade53896b89f09989325e625992579f2717c30b
Israel Guerra <israel.guerra@gmail.com>
2024-10-24 21:11:06 -0300

    adds shorcuts for exiting nvim

--- lua/guerra/plugins/auto-session.lua
+++ lua/guerra/plugins/auto-session.lua
@@ -8,13 +8,13 @@ return {
       auto_session_suppress_dirs = { '~/', '~/downloads/', '~/Downloads', '~/documents' },
     })
 
-    local keymap = vim.keymap
-
     require("which-key").add(
       {
         { "<leader>w",  group = "Session shortcuts" },
         { "<leader>ww", "<cmd>w<CR>",               group = "Save buffer" },
-        { "<leader>wa", "<cmd>wa<CR>",              group = "Save all buffers" },
+        { "<leader>wa", "<cmd>wa<CR>",              desc = "Save all buffers" },
+        { "<leader>wq", "<cmd>qa<CR>",              desc = "Quit all w/o saving" },
+        { "<leader>wx", "<cmd>xa<CR>",              desc = "Quit all saving" },
         { "<leader>wr", "<cmd>SessionRestore<CR>",  desc = "Restore session for cwd" },
         { "<leader>ws", "<cmd>SessionSave<CR>",     desc = "Save session for auto session root dir" },
       }
